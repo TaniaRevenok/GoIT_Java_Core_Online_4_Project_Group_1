e6e94d44de03210fd38a4fa4d49ffc9d84a9bc63
varzar <varzar@i.ua>
2021-12-18 10:41:43 +0200

    modifided HelloProject.java

--- Project_Group_1_varzar/src/main/java/HelloProject.java
+++ Project_Group_1_varzar/src/main/java/HelloProject.java
@@ -1,5 +1,5 @@
 public class HelloProject {
     public static void main(String[] args) {
-        System.out.println("Hello, project! \nHello, team!");
+        System.out.println("Hello, project! \nHello, team!\nTest");
     }
 }
